8c7a28b903357b3aa655ac725b54200a20c17026
FinalProject FE20-2 <148502190+FinalProjectFE20-2@users.noreply.github.com>
2023-10-20 20:10:46 +0300

    Delete config directory

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://fefinalproject:zobmoc-xEzwat-1kexja@cluster0.a0rzitq.mongodb.net/?retryWrites=true&w=majority",
-  secretOrKey: "random very very secret string",
-};
