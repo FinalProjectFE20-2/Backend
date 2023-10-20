17d169b905daf412ae6836a12dcb5b454daabb77
Eugeneowner <ownereugene@gmail.com>
2023-10-20 20:32:02 +0300

    Initial commit

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    `mongodb+srv://fefinalproject:zobmoc-xEzwat-1kexja@cluster0.a0rzitq.mongodb.net/?retryWrites=true&w=majority`,
+  secretOrKey: 'random very very secret string',
+};
